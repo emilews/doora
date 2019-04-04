608e04331302a5f6d434cfa0d04cfd44206893fc
Emilio Wong <emiliowong@MacBook-Air-de-Erick.local>
2019-04-04 00:23:25 -0700

    Can login to the server, added Volley to requests, added unique session code, added internal storage for unique code and some other stuff

--- app/src/main/java/com/csi/csidoora/CONSTANTS.java
+++ app/src/main/java/com/csi/csidoora/CONSTANTS.java
@@ -1,14 +1,63 @@
 package com.csi.csidoora;
 
-public class CONSTANTS {
-    private final String LOGIN_URL = "http://192.168.1.72/csi/login/";
-    private final String CODE_URL = "http://192.168.1.72/csi/get_code/";
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Xml;
+import android.widget.Toast;
 
-    public CONSTANTS(){}
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.UUID;
+
+public class CONSTANTS {
+    //URL
+    private final String LOGIN_URL = "http://192.168.1.72:8000/csi/login/";
+    private final String CODE_URL = "http://192.168.1.72:8000/csi/get_code/";
+    //FILES
+    private final String DEVICE_FILE_NAME = "device.csi";
+    private String DEVICE_CODE = "";
+    private final int DEVICE_CODE_LEN = 36;
+    //Context
+    Context ctx;
+    public CONSTANTS(Context ctx) throws IOException {
+        this.ctx = ctx;
+        try{
+            FileInputStream fi = this.ctx.openFileInput(getDEVICE_FILE_NAME());
+            System.out.println("Found file");
+            int n;
+            StringBuffer sb = new StringBuffer();
+            byte[] data = new byte[DEVICE_CODE_LEN];
+            while ((n = fi.read(data)) != -1) {
+                sb.append(new String(data, 0, n));
+            }
+            DEVICE_CODE = sb.toString();
+        }catch(FileNotFoundException f){
+            try{
+                FileOutputStream fo = this.ctx.openFileOutput(getDEVICE_FILE_NAME(), Context.MODE_PRIVATE);
+                fo.write(createDeviceCode().getBytes());
+                fo.flush();
+                fo.close();
+            }catch(FileNotFoundException fe){
+
+            }
+        }
+    }
     public String getLOGIN_URL(){
         return LOGIN_URL;
     }
     public String getCODE_URL(){
         return CODE_URL;
+    }
+    public String getDEVICE_FILE_NAME(){
+        return DEVICE_FILE_NAME;
+    }
+    private String createDeviceCode(){
+        return UUID.randomUUID().toString();
+    }
+    public String getDEVICE_CODE(){
+        return DEVICE_CODE;
     }
 }
--- app/src/main/java/com/csi/csidoora/MainActivity.java
+++ app/src/main/java/com/csi/csidoora/MainActivity.java
@@ -1,9 +1,12 @@
 package com.csi.csidoora;
 
+import android.Manifest;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,6 +15,7 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -21,11 +25,17 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.github.ybq.android.spinkit.sprite.Sprite;
 import com.github.ybq.android.spinkit.style.CubeGrid;
-import com.github.ybq.android.spinkit.style.DoubleBounce;
-import com.github.ybq.android.spinkit.style.FadingCircle;
-import com.github.ybq.android.spinkit.style.FoldingCube;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.security.Permission;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
 
 
     @Override
@@ -33,10 +43,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button login = findViewById(R.id.loginButton);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.INTERNET},1);
+
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LogIn();
+                try {
+                    LogIn();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -63,23 +82,33 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void LogIn(){
+    public void LogIn() throws IOException {
+        final CONSTANTS constants = new CONSTANTS(this);
+
         EditText e = findViewById(R.id.emailInput);
         EditText p = findViewById(R.id.passwordInput);
         Button b = findViewById(R.id.loginButton);
-        String email = e.getText().toString();
-        String pass = p.getText().toString();
+        final String email = e.getText().toString();
+        final String pass = p.getText().toString();
         e.setVisibility(View.INVISIBLE);
         p.setVisibility(View.INVISIBLE);
         b.setVisibility(View.INVISIBLE);
 
-        CONSTANTS constants = new CONSTANTS();
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.spin_kit);
+        progressBar.setVisibility(View.VISIBLE);
+        Sprite sprite = new CubeGrid();
+        progressBar.setIndeterminateDrawable(sprite);
 
+        final Context ctx = this;
         RequestQueue r = Volley.newRequestQueue(this);
-        StringRequest sr = new StringRequest(Request.Method.GET, constants.getLOGIN_URL(),
+        StringRequest sr = new StringRequest(Request.Method.POST, constants.getLOGIN_URL(),
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        if(response.contains("Success")){
+                            Toast t = Toast.makeText(ctx, "Success!", Toast.LENGTH_LONG);
+                            t.show();
+                        }
 
                     }
                 }, new Response.ErrorListener() {
@@ -88,12 +117,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-        });
+        }){
 
+            protected Map<String, String> getParams() throws com.android.volley.AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("password", pass);
+                params.put("device", constants.getDEVICE_CODE());
+                return params;
+            }
+        };
+        r.add(sr);
+        r.start();
 
-        ProgressBar progressBar = (ProgressBar)findViewById(R.id.spin_kit);
-        progressBar.setVisibility(View.VISIBLE);
-        Sprite sprite = new CubeGrid();
-        progressBar.setIndeterminateDrawable(sprite);
     }
 }
