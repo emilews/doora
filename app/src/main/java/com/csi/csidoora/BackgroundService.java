e054bf3bf2f08a24b7bf66c75efa1f46291f5b57
Emilio Wong <emiliowong@MacBook-Air-de-Erick.local>
2019-04-20 13:42:11 -0700

    Added a method to know if there's a connection

--- app/src/main/java/com/csi/csidoora/BackgroundService.java
+++ app/src/main/java/com/csi/csidoora/BackgroundService.java
@@ -305,6 +305,16 @@ public class BackgroundService extends IntentService {
         r.add(sr);
         r.start();
     }
+    public boolean isOnline() {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        } catch (IOException e)          { e.printStackTrace(); }
+        catch (InterruptedException e) { e.printStackTrace(); }
+        return false;
+    }
 
 }
 
